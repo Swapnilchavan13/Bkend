c13a2ff41bf584e23532039929583600588c263c
swapnil chavan <chavanswapnil822@gmail.com>
2023-09-05 15:42:07 +0530

    Demo

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World! its working fine')
 })
 
+app.get('/data', (req, res) => {
+    res.send("id":1,"title":"iPhone 9","description":"An apple mobile which is nothing like apple","price":549,"discountPercentage":12.96,"rating":4.69,"stock":94,"brand":"Apple","category":"smartphones","thumbnail":"https://i.dummyjson.com/data/products/1/thumbnail.jpg","images":["https://i.dummyjson.com/data/products/1/1.jpg","https://i.dummyjson.com/data/products/1/2.jpg","https://i.dummyjson.com/data/products/1/3.jpg","https://i.dummyjson.com/data/products/1/4.jpg","https://i.dummyjson.com/data/products/1/thumbnail.jpg"]})
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
